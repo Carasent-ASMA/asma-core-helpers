8a0f9dbcf62ceb7f826abaec8bee62a916fbb186
Relaxe111 <40892030+Relaxe111@users.noreply.github.com>
2022-10-26 07:27:27 +0200

    removed lib

--- lib/helpers/InitializeIDBListenersOnMstSnapshots.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function initiatieIDBListenersOnMstSnaphsots<T extends Object, K extends keyof T>(store: T, omit?: K[]): Promise<void>;
-//# sourceMappingURL=InitializeIDBListenersOnMstSnapshots.d.ts.map
