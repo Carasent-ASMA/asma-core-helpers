4353b5d892f54e4151555da846e46a110266b756
Sviataslau <svyatoslav.korshakevich@carasent.com>
2023-01-04 13:29:53 +0300

    fix logout

--- lib/helpers/Config.d.ts
+++ lib/helpers/Config.d.ts
@@ -1,4 +1,4 @@
-type ObjectType<T> = T extends string ? string : T extends boolean ? boolean : never;
+declare type ObjectType<T> = T extends string ? string : T extends boolean ? boolean : never;
 export declare function config<T>(env_var: string, default_value: T): ObjectType<T>;
 export declare function configWeb<T>(env_var: string, default_value: T): ObjectType<T>;
 export declare function httpToWs(url: string): string;
