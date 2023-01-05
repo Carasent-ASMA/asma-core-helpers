c6e2ffd9a3accba3fa66c33bb2044e2167874781
Relaxe111 <40892030+Relaxe111@users.noreply.github.com>
2023-01-05 15:37:12 +0100

    added as generic

--- lib/helpers/generateSrvAuthBindings.d.ts
+++ lib/helpers/generateSrvAuthBindings.d.ts
@@ -1,13 +1,12 @@
 import { AxiosResponse, ResponseType } from 'axios';
 import { EnvironmentEnums } from '..';
-export type feature_names_enum = 'artifact_createQnrCustomContext' | 'autoImportableQnr' | 'calendar_BusyTimesAccess' | 'calendar_CoursesAccess' | 'calendar_EventsAccess' | 'calendar_EventsRequestsAccess' | 'calendar_TasksAccess' | 'calendar_access' | 'calendar_taskTemplatesCRUD' | 'dashboardTraceability' | 'directory_AdvocaCandidatProfile' | 'directory_AdvocaInvormationOnTiltak' | 'documentUploadFromAdopusPicker' | 'documentUploadFromDokkladPicker' | 'documentUploadFromLocalPicker' | 'documentUploadPickedDocuments' | 'experimental' | 'ordersOverviewOnSelectedRecipientsForQnr' | 'predefinedUserForQnr' | 'rejectableQnr' | 'signByTherapistDocument';
-export declare function generateSrvAuthBindings(SRV_AUTH: () => string, DEVELOPMENT: () => boolean, EnvironmentToOperateFn: () => string, logout?: () => void): {
-    hasFeature: (featureName: feature_names_enum) => boolean;
-    getFeatures: () => Set<feature_names_enum> | undefined;
+export declare function generateSrvAuthBindings<FeatureEnums = never>(SRV_AUTH: () => string, DEVELOPMENT: () => boolean, EnvironmentToOperateFn: () => string, logout?: () => void): {
+    hasFeature: (featureName: FeatureEnums) => boolean;
+    getFeatures: () => Set<FeatureEnums> | undefined;
     isJwtValid: () => boolean;
     signin: (url: string, headers?: Record<string, string>) => Promise<{
         token: string;
-        features: feature_names_enum[];
+        features: FeatureEnums[];
     }>;
     srvAuthGet: <R>(url: string, headers?: Record<string, string>) => Promise<AxiosResponse<R, any>>;
     signoutAuth: () => Promise<void>;
