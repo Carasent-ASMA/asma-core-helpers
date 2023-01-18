bf22f5e993cbadf2e8c2fd3d2f625303de22a728
Sviataslau <svyatoslav.korshakevich@carasent.com>
2023-01-18 21:46:10 +0300

    add nullable

--- src/helpers/generateGenqlClient.ts
+++ src/helpers/generateGenqlClient.ts
@@ -15,7 +15,7 @@ export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
 }: {
     // jwt_exp: number
     setReqConfig: () => Promise<AxiosRequestConfig<any>>
-    createClient: (options?: ClientOptions | undefined) => T
+    createClient: (options?: ClientOptions | undefined) => T | null
     serviceUrl: () => string
     path?: string
 }) {
@@ -33,7 +33,7 @@ export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
     //}
 
     async function getGenqlClient() {
-        if (/* accessTokenHasExpired() || */ client === null) {
+        if (/* accessTokenHasExpired() || */ !client) {
             client = await genqlClient()
 
             return client
@@ -59,23 +59,23 @@ export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
      * This is used for anonymous requests as well as authenticated requests
      *
      */
-    async function genqlClient(options: CliOptions = {}): Promise<T> {
-        //let req_headers: Record<string, string> = {}
+    async function genqlClient(options: CliOptions = {}): Promise<T | null> {
+        let req_headers: Record<string, string> = {}
 
         const { anonymous, headers, ...rest } = options
 
         if (!serviceUrl()) {
             console.warn('requred param srv_url is undefined, please check EnvConfig object!')
         }
-        //if (!anonymous) {
-        //req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
-        // setJwtExp(req_headers['Authorization'])
-        // }
+        if (!anonymous) {
+            req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
+            if (!req_headers['Authorization']) return null
+        }
 
         return createClient({
             url: `${serviceUrl()}${path}`,
             headers: async () => ({
-                ...(options.anonymous ? {} : (((await setReqConfig()).headers ?? {}) as Record<string, string>)),
+                ...req_headers,
                 ...headers,
             }),
             batch: { batchInterval: 50, maxBatchSize: 100 },
@@ -85,7 +85,9 @@ export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
 
     async function genqlClientWs() {
         if (/* accessTokenHasExpired() || */ !wsClient) {
-            //const req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
+            const req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
+
+            if (!req_headers['Authorization']) return null
 
             //setJwtExp(req_headers['Authorization'])
 
@@ -96,7 +98,7 @@ export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
                 subscription: {
                     reconnect: true,
                     reconnectionAttempts: 5,
-                    headers: async () => ((await setReqConfig()).headers ?? {}) as Record<string, string>,
+                    headers: async () => req_headers,
                 },
             })
         }
