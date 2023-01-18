cc86663e6833eb318db5fee42039d5880d2baa86
Relaxe111 <40892030+Relaxe111@users.noreply.github.com>
2023-01-18 12:38:58 +0100

    v0.2.31

--- src/helpers/generateGenqlClient.ts
+++ src/helpers/generateGenqlClient.ts
@@ -1,29 +1,39 @@
 import type { AxiosRequestConfig } from 'axios'
-import type { ClientOptions } from '@genql/runtime'
+import type { ClientOptions, createClient } from '@genql/runtime'
 import { httpToWs } from './Config'
+//import { parseJwt } from '../helpers/parseJwt'
 
 interface CliOptions extends Omit<ClientOptions, 'url'> {
     anonymous?: boolean
 }
 
-export function generateGenqlClient<T>({
-    accessTokenHasExpired,
+export function generateGenqlClient<T extends ReturnType<typeof createClient>>({
     setReqConfig,
     createClient,
     serviceUrl,
     path = '/v1/graphql',
 }: {
-    accessTokenHasExpired: () => boolean
+    // jwt_exp: number
     setReqConfig: () => Promise<AxiosRequestConfig<any>>
     createClient: (options?: ClientOptions | undefined) => T
     serviceUrl: () => string
     path?: string
 }) {
+    // let jwt_exp = 0
+
     let client: T | null = null
+
     let wsClient: T | null = null
 
+    //function accessTokenHasExpired() {
+    //    const nowTime = Math.floor(new Date().getTime() / 1000)
+    //
+    //    //set exp time -20sec for token to be refreshed early
+    //    return jwt_exp - 10 <= nowTime
+    //}
+
     async function getGenqlClient() {
-        if (accessTokenHasExpired() || client === null) {
+        if (/* accessTokenHasExpired() || */ client === null) {
             client = await genqlClient()
 
             return client
@@ -32,48 +42,61 @@ export function generateGenqlClient<T>({
         return client
     }
 
-    function resetGenqlClient() {
-        client = null
-    }
+    //function resetGenqlClient() {
+    //    client = null
+    //}
 
+    // function setJwtExp(token?: string) {
+    //     if (!token) return
+    //
+    //     const parsed_jwt = parseJwt<{ exp: number }>(token)?.exp
+    //
+    //     jwt_exp = parsed_jwt || 0
+    // }
+
+    /**
+     *
+     * This is used for anonymous requests as well as authenticated requests
+     *
+     */
     async function genqlClient(options: CliOptions = {}): Promise<T> {
-        let req_headers: Record<string, string> = {}
+        //let req_headers: Record<string, string> = {}
 
         const { anonymous, headers, ...rest } = options
 
         if (!serviceUrl()) {
             console.warn('requred param srv_url is undefined, please check EnvConfig object!')
         }
-        if (!anonymous) {
-            req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
-        }
+        //if (!anonymous) {
+            //req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
+            // setJwtExp(req_headers['Authorization'])
+       // }
 
         return createClient({
             url: `${serviceUrl()}${path}`,
-            headers: {
-                ...req_headers,
+            headers: async () => ({
+                ...(options.anonymous ? {} : (((await setReqConfig()).headers ?? {}) as Record<string, string>)),
                 ...headers,
-            },
+            }),
             batch: { batchInterval: 50, maxBatchSize: 100 },
             ...rest,
         })
     }
 
     async function genqlClientWs() {
-        const req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
+        if (/* accessTokenHasExpired() || */ !wsClient) {
+            //const req_headers = ((await setReqConfig()).headers ?? {}) as Record<string, string>
 
-        if (accessTokenHasExpired() || !wsClient) {
+            //setJwtExp(req_headers['Authorization'])
+
             wsClient = createClient({
                 url: `${httpToWs(serviceUrl())}${path}`,
                 cache: 'reload',
                 batch: { batchInterval: 50, maxBatchSize: 100 },
                 subscription: {
-                    timeout: 1,
                     reconnect: true,
                     reconnectionAttempts: 5,
-                    headers: {
-                        ...req_headers,
-                    },
+                    headers: async () => ((await setReqConfig()).headers ?? {}) as Record<string, string>,
                 },
             })
         }
@@ -81,5 +104,5 @@ export function generateGenqlClient<T>({
         return wsClient
     }
 
-    return { getGenqlClient, resetGenqlClient, genqlClient, genqlClientWs }
+    return { getGenqlClient, /* resetGenqlClient, */ genqlClient, genqlClientWs }
 }
