4e1fd3c253dd22c8daefae589fa873d7b7a59643
igor.efrem <igor.efrem@carasent.com>
2023-03-03 13:36:36 +0200

    v0.4.0

--- lib/helpers/checkForRegisteredSubdomains.d.ts
+++ lib/helpers/checkForRegisteredSubdomains.d.ts
@@ -13,9 +13,13 @@ export declare function onThemeChange(callback: (val: {
 };
 export declare function getTheme(): string;
 export declare function setTheme(theme: string): void;
-export declare function checkForRegisteredSubdomain({ redirect_if_not_exists, setSelectedCustomer, srvAuthGet, }: {
+export declare function checkForRegisteredSubdomain({ redirect_if_not_exists, setSelectedCustomer, srvAuthGet, logos, }: {
     redirect_if_not_exists?: boolean;
     setSelectedCustomer?: (customer_id: string) => void;
     srvAuthGet: <R>(url: string, headers?: Record<string, string> | undefined) => Promise<R>;
-}): Promise<boolean>;
+    logos: {
+        fretexLogo: string;
+        carasentLogo: string;
+    };
+}): Promise<[registeredSubdomain: boolean, unregister: () => void]>;
 //# sourceMappingURL=checkForRegisteredSubdomains.d.ts.map
