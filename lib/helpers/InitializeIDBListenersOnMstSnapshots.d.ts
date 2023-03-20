0ca05eeafaedef198025269c247dd182c1fede7e
igor.efrem <igor.efrem@carasent.com>
2023-03-20 22:39:46 +0100

    updates

--- lib/helpers/InitializeIDBListenersOnMstSnapshots.d.ts
+++ lib/helpers/InitializeIDBListenersOnMstSnapshots.d.ts
@@ -1,4 +1,4 @@
-export declare function initiatieIDBListenersOnMstSnaphsots<T extends Object, K extends keyof T>(store: T, omit?: K[]): {
+export declare function initiateIDBListenersOnMstSnaphsots<T extends Object, K extends keyof T>(store: T, omit?: K[]): {
     idb_check_promise: Promise<void>;
     unregisterAll: () => void;
 };
