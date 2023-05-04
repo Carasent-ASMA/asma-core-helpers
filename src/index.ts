0786d070a3e774132fa89db7277140466d00fbcc
igor.efrem <igor.efrem@carasent.com>
2023-05-04 11:11:05 +0300

    updated package

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 export { getServerErrorMessage, processServerError } from './helpers/ProcessServerError'
-export {
-    CURRENCY_NOK as CURENCY_NOK,
-    NORWEGIAN_PHONE_NUMBER as NORVEGIAN_PHONE_NUMBER,
-    ORDINARY,
-    PERSONAL_NUMBER,
-    YEARS,
-} from './helpers/FormatNumberConstants'
+export { CURRENCY_NOK, NORWEGIAN_PHONE_NUMBER, ORDINARY, PERSONAL_NUMBER, YEARS } from './helpers/FormatNumberConstants'
 export { COUNTRIES, POSTAL_CODE } from './helpers/NorwegianPostalCodes'
 export { capitalizeFirstLetter, toLowercaseAndCapitalizeFirstLetter } from './helpers/CapitalizeFistLetter'
 export * from './helpers/IsAdcuris'
