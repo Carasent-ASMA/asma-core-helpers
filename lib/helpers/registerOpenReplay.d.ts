1e838235a34c1bb6324383d1c92a72f4f67b258e
igor.efrem <igor.efrem@carasent.com>
2023-05-12 00:40:15 +0200

    v0.9.1

---
+++ lib/helpers/registerOpenReplay.d.ts
@@ -0,0 +1,4 @@
+import type Tracker from '@openreplay/tracker';
+export declare let tracker: Tracker;
+export declare function registerOpenReplay(): Promise<void>;
+//# sourceMappingURL=registerOpenReplay.d.ts.map
