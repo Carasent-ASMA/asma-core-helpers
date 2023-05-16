bff7ef275d72083cc7ee27e08e5be12328d0b225
igor.efrem <igor.efrem@carasent.com>
2023-05-16 12:15:17 +0200

    v0.9.8

--- lib/helpers/registerOpenReplay.d.ts
+++ lib/helpers/registerOpenReplay.d.ts
@@ -1,4 +1,22 @@
 import type Tracker from '@openreplay/tracker';
-export declare let tracker: Tracker;
-export declare function registerOpenReplay(): Promise<void>;
+declare const GlobalOpenReplay: {
+    started: boolean;
+    tracker: Tracker | undefined;
+    trackerProfiler: ((name: string) => (fn: Function, thisArg?: any) => any) | undefined;
+    trackerGraphQL: ((_1: string, _2: string, _3: any, result: any) => any) | undefined;
+};
+type IGlobalOpenReplay = Required<typeof GlobalOpenReplay>;
+declare global {
+    interface Window {
+        __ASMA_OPENREPLAY__?: Partial<IGlobalOpenReplay>;
+    }
+}
+export declare function getTrackerModule<Keys extends keyof IGlobalOpenReplay>(name: Keys): Partial<Required<{
+    started: boolean;
+    tracker: Tracker | undefined;
+    trackerProfiler: ((name: string) => (fn: Function, thisArg?: any) => any) | undefined;
+    trackerGraphQL: ((_1: string, _2: string, _3: any, result: any) => any) | undefined;
+}>>[Keys] | undefined;
+export declare function registerOpenReplay(startForSpecificCustomer?: boolean): Promise<void>;
+export {};
 //# sourceMappingURL=registerOpenReplay.d.ts.map
