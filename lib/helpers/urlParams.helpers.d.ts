291ea57ef456296e5dee8ed2129cab7fbc58ef90
igor.efrem <igor.efrem@carasent.com>
2023-05-22 16:45:01 +0200

    added helper metods

---
+++ lib/helpers/urlParams.helpers.d.ts
@@ -0,0 +1,4 @@
+import type { ISearchParams } from 'asma-types';
+export declare function setParamByName(name: ISearchParams, value: string): void;
+export declare function getParamByName(name: string): string;
+//# sourceMappingURL=urlParams.helpers.d.ts.map
