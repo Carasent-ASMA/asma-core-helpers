57dbfe0134d2c41812046e534e8d98850373b975
igor.efrem <igor.efrem@carasent.com>
2023-05-23 09:54:52 +0200

    added helper functions

--- lib/helpers/urlParams.helpers.d.ts
+++ lib/helpers/urlParams.helpers.d.ts
@@ -1,4 +1,10 @@
 import { type ISearchParams, type SearchParamWithValues } from 'asma-types';
-export declare function setParamByName<Key extends ISearchParams>(name: Key, value: (typeof SearchParamWithValues)[Key]): void;
+type ISetParamByName<Key extends ISearchParams> = {
+    name: Key;
+    value: (typeof SearchParamWithValues)[Key];
+};
+export declare function setParamByName<Key extends ISearchParams>(data: ISetParamByName<Key> | ISetParamByName<Key>[]): void;
+export declare function deleteParamByName(name: ISearchParams | ISearchParams[]): void;
 export declare function getParamByName(name: ISearchParams): string;
+export {};
 //# sourceMappingURL=urlParams.helpers.d.ts.map
--- src/helpers/urlParams.helpers.tsx
+++ src/helpers/urlParams.helpers.tsx
@@ -1,12 +1,37 @@
 import { type ISearchParams, type SearchParamWithValues } from 'asma-types'
 import { history } from '../global'
 
-export function setParamByName<Key extends ISearchParams>(name: Key, value: (typeof SearchParamWithValues)[Key]) {
-    const searchParams = new URLSearchParams(history.location.search)
+type ISetParamByName<Key extends ISearchParams> = {
+    name: Key
+    value: (typeof SearchParamWithValues)[Key]
+}
 
-    searchParams.set(name, value)
+export function setParamByName<Key extends ISearchParams>(data: ISetParamByName<Key> | ISetParamByName<Key>[]) {
+    const searchParams = new URLSearchParams(globalThis.location.search)
 
-    history.push(`${window.location.pathname}?${searchParams.toString()}`)
+    if (Array.isArray(data)) {
+        data.forEach((item) => {
+            searchParams.set(item.name, item.value)
+        })
+    } else {
+        searchParams.set(data.name, data.value)
+    }
+
+    history.push(`${globalThis.location.pathname}?${searchParams.toString()}`)
+}
+
+export function deleteParamByName(name: ISearchParams | ISearchParams[]) {
+    const searchParams = new URLSearchParams(globalThis.location.search)
+
+    if (Array.isArray(name)) {
+        name.forEach((item) => {
+            searchParams.delete(item)
+        })
+    } else {
+        searchParams.delete(name)
+    }
+
+    history.push(`${globalThis.location.pathname}?${searchParams.toString()}`)
 }
 
 export function getParamByName(name: ISearchParams) {
