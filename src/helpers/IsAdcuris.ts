2ac1bf7c732d6e9b3aaaf8fd2e59cd00b6d457a8
igor.efrem <igor.efrem@carasent.com>
2023-06-05 17:10:06 +0200

    changed isAduris from const to fn

--- src/helpers/IsAdcuris.ts
+++ src/helpers/IsAdcuris.ts
@@ -1,7 +1,11 @@
 import { getConnectorInternal } from './generateSrvAuthBindings'
 
-export const is_adcuris = isAdcuris()
+let includesAdcurisInHost: boolean | undefined
 /* @__PURE__ */
 export function isAdcuris() {
-    return window.location.host.includes('adcuris') || getConnectorInternal() === 'ADCURIS'
+    if (includesAdcurisInHost === undefined) {
+        includesAdcurisInHost = window.location.host.includes('adcuris')
+    }
+
+    return includesAdcurisInHost || getConnectorInternal() === 'ADCURIS'
 }
