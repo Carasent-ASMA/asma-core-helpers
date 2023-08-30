61fc2972b7e2a14047ef9248a501e7c7709cbf84
Sebastian Darie <sebastian.darie@carasent.com>
2023-08-30 15:38:16 +0000

    Merged in feature/ASMA-2190-update-selected-qnr (pull request #3)
    updated package
    
    * updated package

--- lib/helpers/urlParams.helpers.d.ts
+++ lib/helpers/urlParams.helpers.d.ts
@@ -6,10 +6,11 @@ type ISetParamByName<Key extends ISearchParams> = {
 export declare function setParamByName<Key extends ISearchParams>(data: ISetParamByName<Key> | ISetParamByName<Key>[], del?: ISearchParams | ISearchParams[]): void;
 export declare function deleteParamByName(name: ISearchParams | ISearchParams[]): void;
 export declare function getParamByName<Key extends ISearchParams>(name: Key): {
+    readonly action: string;
     readonly activity_id: string;
     readonly hide: string[];
     readonly selected_patient_id: string;
-    readonly selected_query_id: string;
+    readonly selected_qnr_id: string;
     readonly user_context: import("asma-types/lib").IUserContext;
 }[Key] | null;
 export {};
