543b2ea8b27702f80850db23f7bc400069d25d38
Andrei Grini <andrei.grini@carasent.com>
2023-10-10 14:56:03 +0000

    Merged in feat/added-injectShell__asmaCoreUiStylesheet (pull request #11)
    feat: Added injectShell__asmaCoreUiStylesheet
    
    * feat: Added injectShell__asmaCoreUiStylesheet
    
    * feat: Added injectShell__asmaCoreUiStylesheet

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,7 @@ export * from './helpers/urlParams.helpers'
 export * from './helpers/openReplayConfigs'
 export * from './helpers/sha256'
 export * from './helpers/injectAsmaCoreUiStyles'
+export * from './helpers/injectShell__asmaCoreUiStylesheet'
 
 export * from './utility/fetch'
 
