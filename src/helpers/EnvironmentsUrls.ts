1ceba72a5969c357be10ea05b70f6756167848a2
igor.efrem <igor.efrem@carasent.com>
2024-02-18 14:23:12 +0100

    chore: Refactored generateEnvConfigsBindings

--- src/helpers/EnvironmentsUrls.ts
+++ src/helpers/EnvironmentsUrls.ts
@@ -1,6 +1,39 @@
-import { configWeb, EnvironmentEnums, type IEnvironmentEnums } from '..'
+import { srvConnector, env, env_to_operate, base_url, signicatSignUrl, devExpress, createAdvocaAccessUrl } from '..'
 
-export const EnvironmentsUrls = {
+export const EnvironmentsUrls1 = {
+    SRV_DIRECTORY: `${base_url}/api/directory`,
+    SRV_CALENDAR: `${base_url}/api/calendar`,
+    SRV_PROXY_OLD: `${base_url}/api/proxy`,
+    SRV_PROXY_OLD_HELSE: `${base_url}/api/proxy/helse`,
+    SRV_PROXY_OLD_WEB: `${base_url}/api/proxy/web`,
+    SRV_STORAGE: `${base_url}/api/storage`,
+    SRV_CHAT: `${base_url}/api/chat`,
+    SRV_CONNECTOR: srvConnector(env, env_to_operate),
+    SRV_ARTIFACT: '',
+    SRV_ADVOCA: `${base_url}/api/service`,
+    SRV_PROXY: `${base_url}/api/srvproxy`,
+    SRV_NOTIFICATION: `${base_url}/api/notification`,
+    SRV_ACTIVITIES: `${base_url}/api/activities`,
+
+    SRV_AO_DIRECTORY: `${base_url}/api-ao/directory`,
+    SRV_AO_WRAPPER: '',
+    SRV_AUTH: `${base_url}/api/auth`,
+    DEVELOPMENT: window.location.hostname.includes('dev.') || window.location.hostname.includes('localhost'),
+    ENVIRONMENT_TO_OPERATE: env_to_operate,
+    DEBUG_ADCURIS: false,
+    //SRV_FORWARDER_SECRET: '',
+    SIGNICAT_SIGN_URL: signicatSignUrl(env, env_to_operate),
+    INFO_ADVOCA: 'https://carasent.no/ad-voca/deltaker-bruker',
+    DEVEXPRESS: devExpress(env, env_to_operate),
+    ADVOCA_ACCESS_URL: createAdvocaAccessUrl(),
+    AUTO_SAVE_INTERVAL_IN_MINUTES: 1,
+    ENV: env_to_operate || env,
+
+    API_KEY_GEOCODE: 'AIzaSyAqlT9AT7xDvehIS0XmEXAafzYify5_npg',
+    AZURE_APP_ID: 'cba50bcf-ef6a-4623-8b42-fb592cb064d7',
+    AZURE_APP_REDIRECT_URI: window.location.origin,
+}
+/* export const EnvironmentsUrls = {
     local: {
         SRV_DIRECTORY: `http://${window.location.hostname}:7001`,
         SRV_CALENDAR: `http://${window.location.hostname}:7011`,
@@ -18,23 +51,24 @@ export const EnvironmentsUrls = {
 
         SRV_AO_DIRECTORY: `http://${window.location.hostname}:7013`,
         SRV_AO_WRAPPER: '',
+        SRV_AUTH: '',
     },
     dev: {
-        SRV_DIRECTORY: 'https://dev.adopus.no/api/directory',
-        SRV_CALENDAR: 'https://dev.adopus.no/api/calendar',
-        SRV_PROXY_OLD: 'https://dev.adopus.no/api/proxy',
-        SRV_PROXY_OLD_HELSE: 'https://dev.adopus.no/api/proxy/helse',
-        SRV_PROXY_OLD_WEB: 'https://dev.adopus.no/api/proxy/web',
-        SRV_STORAGE: 'https://dev.adopus.no/api/storage',
-        SRV_CHAT: 'https://dev.adopus.no/api/chat',
+        SRV_DIRECTORY: `${base_url}/api/directory',
+        SRV_CALENDAR: `${base_url}/api/calendar',
+        SRV_PROXY_OLD: `${base_url}/api/proxy',
+        SRV_PROXY_OLD_HELSE: `${base_url}/api/proxy/helse',
+        SRV_PROXY_OLD_WEB: `${base_url}/api/proxy/web',
+        SRV_STORAGE: `${base_url}/api/storage',
+        SRV_CHAT: `${base_url}/api/chat',
         SRV_CONNECTOR: 'https://connector-dev.adopus.no',
         SRV_ARTIFACT: '',
         SRV_ADVOCA: 'https://dev.advoca.no/api/service',
-        SRV_PROXY: 'https://dev.adopus.no/api/srvproxy',
-        SRV_NOTIFICATION: 'https://dev.adopus.no/api/notification',
-        SRV_ACTIVITIES: 'https://dev.adopus.no/api/activities',
+        SRV_PROXY: `${base_url}/api/srvproxy',
+        SRV_NOTIFICATION: `${base_url}/api/notification',
+        SRV_ACTIVITIES: `${base_url}/api/activities',
 
-        SRV_AO_DIRECTORY: 'https://dev.adopus.no/api-ao/directory',
+        SRV_AO_DIRECTORY: `${base_url}/api-ao/directory',
         SRV_AO_WRAPPER: '',
     },
     test: {
@@ -91,9 +125,9 @@ export const EnvironmentsUrls = {
         SRV_AO_DIRECTORY: 'https://www.adopus.no/api-ao/directory',
         SRV_AO_WRAPPER: 'fethes dynamically from srvUrls()',
     },
-}
+} */
 
-export function environmentUrls(ENVIRONMENT_TO_OPERATE?: string) {
+/* export function environmentUrls(ENVIRONMENT_TO_OPERATE?: string) {
     let env: IEnvironmentEnums | undefined
 
     const env_to_operate_window = configWeb('ENVIRONMENT_TO_OPERATE', '')
@@ -108,5 +142,5 @@ export function environmentUrls(ENVIRONMENT_TO_OPERATE?: string) {
     }
 
     return
-}
-export default environmentUrls
+} */
+//export default environmentUrls
