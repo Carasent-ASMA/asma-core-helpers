c61069923a32cec2045bb81f94303541ea91a5bd
igor.efrem <igor.efrem@carasent.com>
2024-04-11 14:04:38 +0200

    chore: Updated type

--- src/helpers/initEnvConfigsVars.ts
+++ src/helpers/initEnvConfigsVars.ts
@@ -156,7 +156,7 @@ export function getOpenReplayKey(journal: string) {
     if (_env === 'prod' && typeof key !== 'string') {
         key = key[_journal]
     }
-    return key
+    return key as string
 }
 
 const { hostname } = window.location
