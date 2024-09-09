247928f67e637e34ac41ca45bac21c78736d4ee8
igor.efrem <igor.efrem@carasent.com>
2024-09-09 23:07:16 +0200

    v0.35.0

--- src/helpers/openReplayConfigs.ts
+++ src/helpers/openReplayConfigs.ts
@@ -1,26 +1,26 @@
-import { realWindow } from '..'
+//import { realWindow } from '..'
 
-let openreplay: IOpenReplayConfig | null = null
+//let openreplay: IOpenReplayConfig | null = null
 
-export type IOpenReplayConfig = {
+/* export type IOpenReplayConfig = {
     enable: boolean
     graphql: boolean
     live_assist: boolean
     mobx: boolean
     profiler: boolean
 }
-
-export function getOpenReplayConfig() {
+ */
+/* export function getOpenReplayConfig() {
     if (realWindow.__ASMA__SHELL__?.openreplay_configs) {
         return realWindow.__ASMA__SHELL__.openreplay_configs
     }
     return openreplay
-}
+} */
 
-export function _setOpenReplayConfig(config: IOpenReplayConfig) {
+/* export function _setOpenReplayConfig(config: IOpenReplayConfig) {
     openreplay = config
 
     realWindow.__ASMA__SHELL__ = realWindow.__ASMA__SHELL__ || {}
 
     realWindow.__ASMA__SHELL__.openreplay_configs = config
-}
+} */
